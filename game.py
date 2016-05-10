2d587f39e2ed8f4d92eaf4714f325e24e130ca9d
BK Bolisetty <bk@squarefruitlabs.com>
2016-05-09 23:49:49 -0500

    Hearts added.

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ import time
 import sys
 
 score = 0
+hearts = 3
 fallback = False
 giant = False
 
@@ -28,37 +29,50 @@ left  = "←         "
 down  = "   ↓      "
 up    = "      ↑   "
 right = "         →"
+heart = " ❤"
 
 if fallback:
-    left  = "<         "
-    down  = "   v      "
-    up    = "      ^   "
-    right = "         >"
+    left  = "<           "
+    down  = "   v        "
+    up    = "      ^     "
+    right = "         >  "
+    heart = " <3"
 
 if giant:
-    left = "  /\n /\n \\\n  \\\n"
-    down = "    \\      /\n     \\    /\n      \\  /\n       \\/\n"
-    up   = "                /\\\n               /  \\\n              /    \\\n             /      \\\n"
-    right= "                      \\\n                       \\\n                       /\n                      /\n"
+    left = "  /\n /\n \\\n  \\                      "
+    down = "    \\      /\n     \\    /\n      \\  /\n       \\/                "
+    up   = "                /\\\n               /  \\\n              /    \\\n             /      \\    "
+    right= "                      \\\n                       \\\n                       /\n                      /  "
+    heart = "<3 "
 
 start = time.time()
 def ddr(dirn):
     if dirn == 'h':
-        print(left)
+        print(left + gethearts(hearts))
     elif dirn == 'j':
-        print(down)
+        print(down + gethearts(hearts))
     elif dirn == 'k':
-        print(up)
+        print(up + gethearts(hearts))
     elif dirn == 'l':
-        print(right)
+        print(right + gethearts(hearts))
+
+def gethearts(num):
+    hc = ""
+    while num > 0:
+        hc = hc + heart
+        num = num - 1
+    return hc
 
 while True:
     direction = random.choice(['h','j','k','l'])
     ddr(direction)
     char = getch.getch()
     if char != direction:
-        break
-    score = score + 1
+        hearts = hearts - 1
+        if hearts == 0:
+            break
+    else:
+        score = score + 1
 end = time.time()
 time = round(end - start, 2)
 print("Time : ", str(time)+"s")

b8ae1c5228538b86ad80197a9576e68b7911afc1
BK Bolisetty <bk@blitzkraft.me>
2018-05-02 16:06:37 -0400

    Bring up pylint score
    Initial version isn't very good at pylint score. Now it's a lot better.
    Currently at 9.53. Only two lines disabled inline. Will need to be fixed
    sometime.

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
-
+"""
 # For best results use source code pro
 # Arrows
 # "←..↑..↓..→"
+"""
 
 import datetime
 import random
@@ -16,126 +17,149 @@ except ImportError:
 import time
 import sys
 
-score = 0
-accu = 0
-hearts = 3
-streak = 0
-long_streak = 0
-fallback = False
-giant = False
-inf = False
-gtr = False
-gtro = False
 
 try:
-    mode = sys.argv
+    MODE = sys.argv
 except IndexError:
-    mode = 'default'
+    MODE = 'default'
 
-if 'help' in mode:
-    print("Usage: ./game.py [ fallback | giant ] [ inf ] ")
-    print("See README for more details.")
-    sys.exit()
+class GameEnv(): # pylint: disable=too-many-instance-attributes, too-few-public-methods
+    """
+    Start up variables
+    """
+    score = 0
+    accu = 0
+    hearts = 3
+    streak = 0
+    long_streak = 0
+    fallback = False
+    giant = False
+    g = False
+    gtr = False
+    gtro = False
 
-total = 0
-if 'inf' in mode:
-    inf = True
-    hearts = 0
-if 'fallback' in mode: 
-    fallback = True
-elif 'giant' in mode:
-    giant = True
-elif 'gtr' in mode:
-    gtr = True
-elif 'gtro' in mode:
-    gtro = True
+GAME_SETTINGS = GameEnv()
 
-left  = "←         "
-down  = "   ↓      "
-up    = "      ↑   "
-right = "         →"
-heart = " ❤"
+def game():
+    """
+    Game logic
+    """
+    if 'help' in MODE:
+        print("Usage: ./game.py [ fallback | giant ] [ g ] ")
+        print("See README for more details.")
+        sys.exit()
 
-if fallback:
-    left  = "<           "
-    down  = "   v        "
-    up    = "      ^     "
-    right = "         >  "
-    heart = " <3"
+    total = 0
+    if 'g' in MODE:
+        GAME_SETTINGS.g = True # pylint: disable=invalid-name
+        GAME_SETTINGS.hearts = 0
+    if 'fallback' in MODE:
+        GAME_SETTINGS.fallback = True
+    elif 'giant' in MODE:
+        GAME_SETTINGS.giant = True
+    elif 'gtr' in MODE:
+        GAME_SETTINGS.gtr = True
+    elif 'gtro' in MODE:
+        GAME_SETTINGS.gtro = True
 
-if giant:
-    left = "  /\n /\n \\\n  \\                      "
-    down = "    \\      /\n     \\    /\n      \\  /\n       \\/                "
-    up   = "                /\\\n               /  \\\n              /    \\\n             /      \\    "
-    right= "                      \\\n                       \\\n                       /\n                      /  "
-    heart = "<3 "
-
-if gtr:
-    left  = "\033[1;31;49mH\033[1;30m  |  |  |  "
-    down  = "|  \033[1;32;49mJ\033[1;30m  |  |  "
-    up    = "|  |  \033[1;33;49mK\033[1;30m  |  "
-    right = "|  |  |  \033[1;34;49mL\033[1;30m  "
-    heart = " <3"
-
-
-if gtro:
-    left  = "\033[1;31;49mO\033[1;30m  |  |  |  "
-    down  = "|  \033[1;32;49mO\033[1;30m  |  |  "
-    up    = "|  |  \033[1;33;49mO\033[1;30m  |  "
-    right = "|  |  |  \033[1;34;49mO\033[1;30m  "
-    heart = " <3"
-
-
-start = time.time()
-def ddr(dirn):
-    if dirn == 'h':
-        print(left + gethearts(hearts))
-    elif dirn == 'j':
-        print(down + gethearts(hearts))
-    elif dirn == 'k':
-        print(up + gethearts(hearts))
-    elif dirn == 'l':
-        print(right + gethearts(hearts))
-
-def gethearts(num):
-    hc = ""
-    while num > 0:
-        hc = hc + heart
-        num = num - 1
-    return hc
-
-while True:
-    direction = random.choice(['h','j','k','l'])
-    ddr(direction)
-    char = msvcrt.getch().lower()
-    #if inf:
-    total = total + 1
-    if char == 'q':
-        break
-    elif char != direction:
-        long_streak = max(streak, long_streak)
-        streak = 0
-        hearts = hearts - 1
-        if hearts == 0:
+    heart = " ❤"
+    start = time.time()
+    while True:
+        direction = random.choice(['h', 'j', 'k', 'l'])
+        ddr(direction, GAME_SETTINGS.hearts, heart)
+        char = msvcrt.getch().lower()
+        total = total + 1
+        if char == 'q':
             break
-    else:
-        streak = streak + 1
-        score = score + 1
+        elif char != direction:
+            GAME_SETTINGS.long_streak = max(GAME_SETTINGS.streak, GAME_SETTINGS.long_streak)
+            GAME_SETTINGS.streak = 0
+            GAME_SETTINGS.hearts = GAME_SETTINGS.hearts - 1
+            if GAME_SETTINGS.hearts == 0:
+                break
+        else:
+            GAME_SETTINGS.streak = GAME_SETTINGS.streak + 1
+            GAME_SETTINGS.score = GAME_SETTINGS.score + 1
 
-def record_scores():
+    end = time.time()
+    time_completion = round(end - start, 2)
     date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     user = getpass.getuser()
-    global accu
-    accu = str(round(score * 100/total, 2))+ '%'
+    GAME_SETTINGS.accu = str(round(GAME_SETTINGS.score * 100/total, 2))+ '%'
+    record_scores(user, date, GAME_SETTINGS.accu, GAME_SETTINGS.score, GAME_SETTINGS.long_streak)
+    print("Time : ", str(time_completion)+"s")
+    print("Accu : ", GAME_SETTINGS.accu, "(", GAME_SETTINGS.score, "of", total, ")")
+    print("Speed: ", round(GAME_SETTINGS.score/time_completion, 2), "strokes/sec")
+    print("Longest streak: ", GAME_SETTINGS.long_streak)
+
+def record_scores(user, date, accu, score, long_streak):
+    """
+    Score is recorded to scores.txt
+    """
     outstr = "\t".join([user, str(score), accu, str(long_streak), str(time) + "s\t", date])
     with open("scores.txt", "a") as score_file:
         score_file.write(outstr + "\n")
 
-end = time.time()
-time = round(end - start, 2)
-record_scores()
-print("Time : ", str(time)+"s")
-print("Accu : ", accu, "(", score, "of", total, ")")
-print("Speed: ", round(score/time,2), "strokes/sec")
-print("Longest streak: ", long_streak)
+def gethearts(num, heart):
+    """
+    Hearts are managed here
+    """
+    heart_count = ""
+    while num > 0:
+        heart_count = heart_count + heart
+        num = num - 1
+    return heart_count
 
+def ddr(dirn, hearts, heart):
+    """
+    DDR is printed here
+    """
+    left = "←         "
+    down = "   ↓      "
+    up_up = "      ↑   "
+    right = "         →"
+    heart = " ❤"
+
+    if GAME_SETTINGS.fallback:
+        left = "<           "
+        down = "   v        "
+        up_up = "      ^     "
+        right = "         >  "
+        heart = " <3"
+
+    if GAME_SETTINGS.giant:
+        left = "  /\n /\n \\\n  \\                      "
+        down = "    \\      /\n     \\    /\n      \\  /\n       \\/                "
+        up_up = "                /\\\n               /  \\\n              /    \\\n             /      \\    "
+        right = "                      \\\n                       \\\n                       /\n                      /  "
+        heart = "<3 "
+
+    if GAME_SETTINGS.gtr:
+        left = "\033[1;31;49mH\033[1;30m  |  |  |  "
+        down = "|  \033[1;32;49mJ\033[1;30m  |  |  "
+        up_up = "|  |  \033[1;33;49mK\033[1;30m  |  "
+        right = "|  |  |  \033[1;34;49mL\033[1;30m  "
+        heart = " <3"
+
+
+    if GAME_SETTINGS.gtro:
+        left = "\033[1;31;49mO\033[1;30m  |  |  |  "
+        down = "|  \033[1;32;49mO\033[1;30m  |  |  "
+        up_up = "|  |  \033[1;33;49mO\033[1;30m  |  "
+        right = "|  |  |  \033[1;34;49mO\033[1;30m  "
+        heart = " <3"
+
+
+    if dirn == 'h':
+        print(left+ gethearts(hearts, heart))
+    elif dirn == 'j':
+        print(down+ gethearts(hearts, heart))
+    elif dirn == 'k':
+        print(up_up + gethearts(hearts, heart))
+    elif dirn == 'l':
+        print(right + gethearts(hearts, heart))
+
+
+
+if __name__ == "__main__":
+    game()
